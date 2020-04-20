a020d157bdad2f092cb13d4b2cc04717b3319a4a
Arjun Savel <asavel@berkeley.edu>
2020-04-20 15:50:08 -0700

    added webb_approved test

--- webb_scraping/tests/tests_1.py
+++ webb_scraping/tests/tests_1.py
@@ -4,7 +4,6 @@ sys.path.append(os.getcwd()[:-6])
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import unittest
-import numpy as np
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 # sys.path.insert(0, os.getcwd()) 
@@ -12,11 +11,29 @@ parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 # current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 
-
 class TestImports(unittest.TestCase):
 
 	def test_calculations_import(self):
-		from webb_scraping import calculations
+		import webb_scraping.calculations as c
 
 	def test_target_import(self):
-		from webb_scraping import target
+		import webb_scraping.target as t
+
+class TestWebb(unittest.TestCase):
+	import webb_scraping.target as t
+	import webb_scraping.calculations as c
+
+	def test_webb_approved(self):
+		"""
+		Take a random assortment of stars and ensure that they are
+		webb-approved if they are supposed to be.
+		"""
+		stars = ['GJ 357']
+		stars_webb_approved = []
+		expected_stars_webb_approved = [True]
+		for star in stars:
+			star_target = t.Target(star)
+			star_target.scape_webb_MAST()
+			stars_webb_approved.append(star_target.webb_approved)
+
+		self.assertItemsEqual(stars_webb_approved, expected_stars_webb_approved)
