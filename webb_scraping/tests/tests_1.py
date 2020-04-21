61025bfdda7dd02a6f1e48c00dbc215da02e4345
Arjun Savel <asavel@berkeley.edu>
2020-04-20 19:16:57 -0700

    added hst_approved test case

--- webb_scraping/tests/tests_1.py
+++ webb_scraping/tests/tests_1.py
@@ -37,3 +37,20 @@ class TestWebb(unittest.TestCase):
 			stars_webb_approved.append(star_target.webb_approved)
 
 		self.assertListEqual(stars_webb_approved, expected_stars_webb_approved)
+
+	def test_hst_approved(self):
+		"""
+		Take a random assortment of stars and ensure that they are
+		webb-approved if they are supposed to be.
+		"""
+		import webb_scraping.target as t
+		import webb_scraping.calculations as c
+		stars = ['TRAPPIST-1']
+		stars_hst_approved = []
+		expected_stars_hst_approved = [True]
+		for star in stars:
+			star_target = t.Target(star)
+			star_target.scrape_HST()
+			stars_webb_approved.append(star_target.hst_approved)
+
+		self.assertListEqual(stars_hst_approved, expected_stars_hst_approved)
